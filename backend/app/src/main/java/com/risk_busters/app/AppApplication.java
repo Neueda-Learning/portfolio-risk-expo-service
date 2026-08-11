eb08bebf9acb151fdd734f52441686e072080239
niemiecdawidit <niemiecdawidit@gmail.com>
2026-08-11 08:16:03 +0000

    Web API demo - hello world

--- backend/app/src/main/java/com/risk_busters/app/AppApplication.java
+++ backend/app/src/main/java/com/risk_busters/app/AppApplication.java
@@ -3,13 +3,21 @@ package com.risk_busters.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 //temporary db fix
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
