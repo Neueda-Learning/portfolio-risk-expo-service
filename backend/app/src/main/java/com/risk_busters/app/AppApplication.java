fb413d8266c96dd75f16f2e3dce11c51c05996eb
niemiecdawidit <niemiecdawidit@gmail.com>
2026-08-11 07:49:02 +0000

    Added temporary annotation to make app run without database configured yet

--- backend/app/src/main/java/com/risk_busters/app/AppApplication.java
+++ backend/app/src/main/java/com/risk_busters/app/AppApplication.java
@@ -2,8 +2,10 @@ package com.risk_busters.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//temporary db fix
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AppApplication {
 
 	public static void main(String[] args) {
